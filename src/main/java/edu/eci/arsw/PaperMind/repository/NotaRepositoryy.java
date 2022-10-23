3a8fec810e69ad4d23243610ee9505605be29b14
liontama2121 <juancamolina9721@gmail.com>
2022-10-22 23:02:02 -0500

    cambio

--- src/main/java/edu/eci/arsw/PaperMind/repository/NotaRepositoryy.java
+++
@@ -1,9 +0,0 @@
-package edu.eci.arsw.PaperMind.repository;
-
-import edu.eci.arsw.PaperMind.model.Nota;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotaRepositoryy extends JpaRepository<Nota, Long> {
-}
