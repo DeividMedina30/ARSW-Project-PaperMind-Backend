ecc4e93e48e3eb537bf6b9f596e6d151b811b907
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2022-11-02 21:05:55 -0500

    cROOS

--- src/main/java/edu/eci/arsw/PaperMind/backend/config/WebSocketConfig.java
+++ src/main/java/edu/eci/arsw/PaperMind/backend/config/WebSocketConfig.java
@@ -2,12 +2,14 @@ package edu.eci.arsw.PaperMind.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@CrossOrigin(origins = "*")
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
@@ -18,6 +20,6 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stompBiblioteca").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/getBibliotecas").setAllowedOriginPatterns("*").withSockJS();
     }
 }
