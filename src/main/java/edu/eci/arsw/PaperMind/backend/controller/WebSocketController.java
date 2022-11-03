7e6fc0b49b377bc7eb7874acba099d31167a69c0
DeividMedina30 <59977494+DeividMedina30@users.noreply.github.com>
2022-11-03 13:34:41 -0500

    Update WebSocketController.java

--- src/main/java/edu/eci/arsw/PaperMind/backend/controller/WebSocketController.java
+++ src/main/java/edu/eci/arsw/PaperMind/backend/controller/WebSocketController.java
@@ -8,7 +8,6 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
-@CrossOrigin(origins = "*")
 public class WebSocketController {
 
     @Controller
