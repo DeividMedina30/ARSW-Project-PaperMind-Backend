225811811c8d8d33b3cb2179d7b4953b5095241c
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2022-11-04 18:24:17 -0500

    Probando conexión

--- src/main/java/edu/eci/arsw/PaperMind/backend/controller/WebSocketController.java
+++ src/main/java/edu/eci/arsw/PaperMind/backend/controller/WebSocketController.java
@@ -6,11 +6,13 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 public class WebSocketController {
 
     @Controller
+    @CrossOrigin(origins = "*")
     public class STOMPMessagesHandler {
 
         @Autowired
