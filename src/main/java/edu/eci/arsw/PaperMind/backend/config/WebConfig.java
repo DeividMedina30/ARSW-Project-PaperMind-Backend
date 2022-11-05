25b9cd0f9082eee6fc78fac032265bafd07ecf93
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2022-11-04 19:02:31 -0500

    Probando conexión

--- src/main/java/edu/eci/arsw/PaperMind/backend/config/WebConfig.java
+++ src/main/java/edu/eci/arsw/PaperMind/backend/config/WebConfig.java
@@ -1,34 +1,17 @@
 package edu.eci.arsw.PaperMind.backend.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
+public class WebConfig implements WebMvcConfigurer {
     @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.cors().configurationSource(new CorsConfigurationSource() {
-            @Override
-            public CorsConfiguration getCorsConfiguration(ServerWebExchange exchange) {
-                return new CorsConfiguration().applyPermitDefaultValues();
-            }
-
-            @Override
-            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                return new CorsConfiguration().applyPermitDefaultValues();
-            }
-        });
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true);
     }
 }
