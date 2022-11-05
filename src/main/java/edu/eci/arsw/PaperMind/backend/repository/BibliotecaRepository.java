a15278a5c3d39f62dbbb884afd1016a93a92de73
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2022-11-05 11:08:05 -0500

    Probando GetName

--- src/main/java/edu/eci/arsw/PaperMind/backend/repository/BibliotecaRepository.java
+++ src/main/java/edu/eci/arsw/PaperMind/backend/repository/BibliotecaRepository.java
@@ -4,6 +4,7 @@ package edu.eci.arsw.PaperMind.backend.repository;
 import edu.eci.arsw.PaperMind.backend.model.Biblioteca;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.List;
 @Repository //componente encargado de resolver el acceso a los datos de nuestro micro-servicio.
 public interface BibliotecaRepository extends JpaRepository<Biblioteca, Long> {
 
-    @Query(value = "SELECT b FROM bibliotecas b WHERE b.nombre = ?1")
-    Biblioteca findByName(String name);
+    @Query(value = "SELECT b FROM bibliotecas b WHERE b.nombre = :nombre")
+    Biblioteca findByName(@Param("nombre") String nombre);
 
 }
