bcdf1cdb5fe9b4bb6c8a98fcb31d3048e9a4ad36
DeividMedina30 <deivid.medina@mail.escuelaing.edu.co>
2022-11-22 11:21:27 -0500

    Realizando Prueba del Controller

--- src/test/java/edu/eci/arsw/PaperMind/backend/repository/BibliotecaRepositoryTests.java
+++ src/test/java/edu/eci/arsw/PaperMind/backend/repository/BibliotecaRepositoryTests.java
@@ -1,4 +1,24 @@
 package edu.eci.arsw.PaperMind.backend.repository;
 
+import edu.eci.arsw.PaperMind.backend.model.Biblioteca;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.persistence.EntityManager;
+
+@DataJpaTest //proporciona una forma conveniente de configurar un entorno con una base de datos integrada para probar nuestras consultas de base de datos.
 public class BibliotecaRepositoryTests {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Autowired
+    private BibliotecaRepository bibliotecaRepository;
+
+    @Test
+    void testFindByName(){
+        Biblioteca biblioteca = new Biblioteca();
+        
+    }
 }
